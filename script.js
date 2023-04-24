b85a3f3f1fed06916f05f7fadc12e83655f7b75e
lmusse4 <127805935+lmusse4@users.noreply.github.com>
2023-04-23 21:32:29 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+//API Key 
+const apiKey = "154c398e47f400515629de78b8b0ec25";
+
+//DOM elements
+const zipInput = document.getElementById("zipcode");
+const submitInfo = document.getElementById("submit");
+const weatherInfo = document.getElementById("weather-info");
+
+// Function to give weather information
+function giveWeatherInfo(city, date, tempHigh, tempLow, description, currentTemp) {
+  // Create new DOM elements
+  const heading = document.createElement("h2");
+  const dateElement = document.createElement("p");
+  const temperature = document.createElement("h1");
+  const highTemperature = document.createElement("p");
+  const lowTemperature = document.createElement("p");
+  const descriptionElement = document.createElement("p");
+
+  // Text of my new elements
+  heading.textContent = city;
+  dateElement.textContent = date;
+  temperature.textContent = `${currentTemp}°F` ;
+  highTemperature.textContent = `High: ${tempHigh}\°F`;
+  lowTemperature.textContent = `Low: ${tempLow}\°F`;
+  descriptionElement.textContent = description;
+
+  // Remove any existing weather information
+  while (weatherInfo.firstChild) {
+    weatherInfo.removeChild(weatherInfo.firstChild);
+  }
+
+  // new elements to the weatherInfo element
+  weatherInfo.appendChild(heading);
+  weatherInfo.appendChild(dateElement);
+  weatherInfo.appendChild(temperature);
+  weatherInfo.appendChild(highTemperature);
+  weatherInfo.appendChild(lowTemperature);
+  weatherInfo.appendChild(descriptionElement);
+}
+
+// Define function to get weather information
+function getWeatherInfo() {
+  const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zipInput.value}&appid=${apiKey}&units=imperial`;
+
+  // Fetch weather data from the API
+  fetch(url)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      const city = data.name;
+      const date = new Date(data.dt * 1000).toLocaleDateString();
+      const tempHigh = data.main.temp_max;
+      const tempLow = data.main.temp_min;
+      const description = data.weather[0].description;
+      const currentTemp = data.main.temp;
+
+      // Give weather information 
+      giveWeatherInfo(city, date, tempHigh, tempLow, description, currentTemp);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
+
+// Event listener to submit button
+submitInfo.addEventListener("click", getWeatherInfo);
